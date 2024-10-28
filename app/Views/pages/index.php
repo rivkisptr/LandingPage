8f4f0b18da45dd6d5347c03e7798981702a118ad
RizkyIcwan <rizkyicwan84@gmail.com>
2024-10-28 11:12:50 +0700

    Add section jakselapp dan img

--- app/Views/pages/index.php
+++ app/Views/pages/index.php
@@ -16,7 +16,7 @@
                 <?php foreach ($kecamatan as $k): ?>
                     <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item isotope-item" data-bs-toggle="modal" data-bs-target="#KecamatanBackdrop-<?= $k['slug']; ?>">
                         <div class="portfolio-content h-100">
-                            <a data-gallery="portfolio-gallery-app"><img src="assets/img/default_img.jpg" class="img-fluid" alt=""></a>
+                            <a data-gallery="portfolio-gallery-app"><img src="assets/img/kecamatan.png" class="img-fluid" alt=""></a>
                             <div class="portfolio-info">
                                 <h4><a href="" title="More Details">Kecamatan <?= $k['nama_kecamatan']; ?></a></h4>
                             </div>
@@ -46,7 +46,7 @@
                                             <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item isotope-item">
                                                 <div class="portfolio-content h-100">
                                                     <a href="<?= $k['link']; ?>" data-gallery="portfolio-gallery-app">
-                                                        <img src="assets/img/default_img.jpg" class="img-fluid" alt="">
+                                                        <img src="assets/img/kecamatan.png" class="img-fluid" alt="">
                                                     </a>
                                                     <div class="portfolio-info">
                                                         <h4><a title="More Details"><?= $k['nama_kecamatan']; ?></a></h4>
@@ -66,7 +66,7 @@
                                                     <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item isotope-item">
                                                         <div class="portfolio-content h-100">
                                                             <a href="<?= $kel['link']; ?>" data-gallery="portfolio-gallery-app">
-                                                                <img src="assets/img/default_img.jpg" class="img-fluid" alt="">
+                                                                <img src="assets/img/kelurahan.png" class="img-fluid" alt="">
                                                             </a>
                                                             <div class="portfolio-info">
                                                                 <h4><a title="More Details"><?= $kel['nama_kelurahan']; ?></a></h4>
@@ -89,6 +89,171 @@
                     </div>
                 <?php endforeach; ?>
 
+    </section>
+    <section>
+        <div class="container justify-content-center">
+            <h1>Jaksel Apps</h1>
+            <div class="box">
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Website-Kota-Adm-Jakarta-Selatan.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Website Kota Adm Jakarta Selatan</h4>
+                        <p>
+                            Media Informasi Kota Adm Jakarta Selatan yang terdiri dari Berita,
+                            <br>Agenda, Data dan Informasi
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/PPID-Jakarta-Selatan.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>PPID Jakarta Selatan</h4>
+                        <p>
+                            Website Pejabat Pengelola Informasi dan Dokumentasi (PPID)
+                            <br>Sekretariat Kota Administrasi Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/TechnoCare.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Technocare LTIK</h4>
+                        <p>
+                            Layanan Help Desk Sudin Kominfotik
+                            <br>Kota Adminitrasi Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Ekin-PJLP-Setko.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Ekin PJLP Setko</h4>
+                        <p>
+                            Sistem pencatatan aktivitas kerja PJLP Setko
+                            <br>Kota Adminitrasi Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Sinoli.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Sinoli</h4>
+                        <p>
+                            Sistem Informasi non Litigasi Bagian Hukum
+                            <br>Kota Adminitrasi Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Siperbast.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Siperbast</h4>
+                        <p>
+                            Sistem Informasi Penerbitan Berita Acara
+                            <br>Serah Terima Bagian PLH
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/E-Kinerja-PJLP.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>E-Kinerja PJLP</h4>
+                        <p>
+                            Sistem pencatatan aktivitas kerja PJLP
+                            <br>Sudin Kominfotik Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Posko-Siaga-Bencana.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Posko Siaga Bencana</h4>
+                        <p>
+                            Sistem pencatatan segala keperluan dan bantuan
+                            <br>yang dibutuhkan dalam bencana
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Destinasi-Jakarta-Selatan.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Destinasi Jakarta Selatan</h4>
+                        <p>
+                            Peta destinasi pariwisata di Jakarta Selatan
+                        </p>
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="imgbx">
+                        <img src="/assets/img/Website-Bagian-Hukum.png" alt="img">
+                    </div>
+                    <div class="content">
+                        <h2 class="link">
+                            <a href="URL_YANG_DIINGINKAN" class="button">Kunjungi</a>
+                        </h2>
+                        <h4>Website Bagian Hukum</h4>
+                        <p>
+                            Dokumentasi kegiatan bagian hukum
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </main>
 <!-- Scroll Tap -->
